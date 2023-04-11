f9d07a5939abdfe98500818e3dbb22664e7e1a16
devoloper86 <obidjondevoloper@gmail.com>
2023-04-11 11:24:29 +0500

    rirst react project

---
+++ src/Navbar/Navbar-4/Navbar_four.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Navbar_four() {
+  return (
+    <div className="w-full h-[50px] bg-slate-900 flex  justify-around">
+      <div className="border-b-4 border-transparent flex items-center cursor-pointer  hover:border-white">
+        <p className="text-white font-bold text-xs ">
+          Furniture
+        </p>
+      </div>
+      <p className="text-white font-bold text-xs">Outdoor</p>
+      <p className="text-white font-bold text-xs">Tabletop & Bar</p>
+      <p className="text-white font-bold text-xs">Kitchen</p>
+      <p className="text-white font-bold text-xs">Bedding & Bath</p>
+      <p className="text-white font-bold text-xs">Decor & Pillows</p>
+      <p className="text-white font-bold text-xs">Rugs</p>
+      <p className="text-white font-bold text-xs">Lighting</p>
+      <p className="text-white font-bold text-xs">Window</p>
+      <p className="text-white font-bold text-xs">Gifts</p>
+      <p className="text-rose-600 font-bold text-xs">SALE</p>
+      <p className="text-white font-bold text-xs">&kids</p>
+    </div>
+  );
+}
+
+export default Navbar_four;
